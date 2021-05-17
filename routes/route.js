75861fbfbc26f51712d842121d2aaefa1035fb0a
Amanda Ualt <nobre@alunos.utfpr.edu.br>
2021-05-16 22:22:40 -0300

    substituição de barra de select por barra de pesquisa

--- routes/route.js
+++ routes/route.js
@@ -1,15 +1,20 @@
-let http = require('http'),
-    express = require('express'),
-    app = express(),
-    Facts = require('../model/facts');
+let http = require("http"),
+  express = require("express"),
+  app = express(),
+  Facts = require("../model/facts");
 
-app.get('/facts', async (req, res)=>{
-    const facts = await Facts.connect();
-    facts.collection('facts').find().toArray((err, resp)=>{
-        if(err) throw err
-        console.log(resp)
-       res.json(resp)
-    })
+app.get("/facts", async (req, res) => {
+  const facts = await Facts.connect();
+  const busca = req.query.busca,
+    posts = await Facts.find(busca);
+  facts
+    .collection("facts")
+    .find()
+    .toArray((err, resp) => {
+      if (err) throw err;
+      console.log(resp);
+      res.json(resp);
+    });
 });
 
 app.listen(3000);
