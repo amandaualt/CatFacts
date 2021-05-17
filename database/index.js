947e309438e9c6f16ef8113043eb5772c4f330fb
MaCandido <oliveiracandidomatheus@gmail.com>
2021-05-16 21:56:31 -0300

    cadastro de usuario, login e autenticação

--- database/index.js
+++
@@ -1,9 +0,0 @@
-const moongoose = require('mongoose');
-
-moongoose.connect('mongodb://localhost:27017/animalFacts', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
-moongoose.Promise = global.Promise;
-
-module.exports = moongoose;
