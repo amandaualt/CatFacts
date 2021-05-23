c8653c29eff404c4a47c3410be9cb3c3312b0e86
MaCandido <oliveiracandidomatheus@gmail.com>
2021-05-23 20:11:18 -0300

    variavel de porta

--- src/index.js
+++ src/index.js
@@ -46,5 +46,5 @@ app.use((req, res, next) => {
 
 require("./app/controllers/index")(app);
 
-//app.listen(process.env.PORT || 3000);
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
+// app.listen(3000);
